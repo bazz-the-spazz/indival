5ab012dbda82c3ee2a4aa0dc07c615b65cbe78b5
bazz-the-spazz <44613732+bazz-the-spazz@users.noreply.github.com>
2022-10-11 13:28:52 +0200

    Update Example.r

--- Example.r
+++ Example.r
@@ -1,8 +1,8 @@
 
 # # Example
 
-#1. Run the scripts to get the functions and the source data:
-
+# 1. 
+# Run the scripts to get the functions and the source data:
 source("Calculate_indicator_values.r")
 source("Correct_species_names.r")
 source("read_data_from_Vegedaz.r")
@@ -12,7 +12,7 @@ source("read_data_from_Vegedaz.r")
 # read data from Vegedaz (Flora Indicativa, Landolt)
 # path=("~/.wine/drive_c/Program Files/Vegedaz/Daten/") # on Linux
 # path=("C:\Program Files\Vegedaz\Daten\") # or wherever Vegedaz is located on Windows
-vegedaz <- read.vegedaz.data(path = "~/.wine/drive_c/Program Files/Vegedaz/Daten/")
+vegedaz <- read.vegedaz.data(path = "C:\Program Files\Vegedaz\Daten\")
 
 # 1.2
 # for Veg (Flora Helvetica 2014), extract the Zip file and put the file "Zeigerliste.txt" into your work directory.
@@ -22,25 +22,28 @@ source("read_data_from_Veg_2015.r")
 ## choose your data source to proceed
 source <- vegedaz$indicativa   # the options are 'vegedaz$landolt', 'vegedaz$indicativa' from Vegedaz, or 'floraH' from Veg
 
-#2.1 for the exercise  create a random dataframe (with species as rows and plots as columns)
+# 2.
+# for the exercise create a random dataframe (with species as rows and plots as columns)
 
+# 2.1
 species <- c("Daucus carota",  "Lathyrus pratensis", "Scorzoneroides autumnalis", "Aegopodiu podagraria", "Leucanthemum vulgaris", "Erigeron annuus")
 d <- data.frame(species=species, plotA= runif(length(species)), plotB= runif(length(species)), plotC= runif(length(species)))
 d
 
-#2.2 transpose data.frame for the analyses (species as columns and plots as rows)
-
+# 2.2 transpose the data.frame for the analyses (species as columns and plots as rows)
 row.names(d) <- d$species
 d$species <- NULL
 d <- as.data.frame(t(d))
+d
 
-#3. Use the choose.name()-function to correct the species names
+# 3. 
+# Use the choose.name()-function to correct the species names according to the chosen source
+names(d) <- corrected.names <- choose.name(names = names(d), data = source)
+corrected.names
+d
 
-corrected.names <- choose.name(names = names(d), data = source)
-names(d) <- corrected.names
-
-#4. Use the get.indicator.value()-function to calculate mean e.g. Temperaturzahl for the three plots
-
+# 4. 
+# Use the get.indicator.value()-function to calculate average e.g. Temperaturzahl for the plots
 get.indicator.value(d=d, value = "Temperaturzahl", data = source, socio = T, propose.alternatives = T) # *
 get.indicator.value(d=d, value = "Temperaturzahl", data = source, method = "sd")
 
