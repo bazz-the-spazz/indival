e3774e557145f7546cc56acaa5b064dcb9ba14c2
bazz-the-spazz <44613732+bazz-the-spazz@users.noreply.github.com>
2022-10-11 12:26:16 +0200

    Update Correct_species_names.r

--- Correct_species_names.r
+++ Correct_species_names.r
@@ -1,4 +1,4 @@
-# Functions to align your Species with Vegedaz 
+# Functions to align your Species with Vegedaz
 
 
 # Function 1: test for correct names (not very good but quick) for better Results use the following function choose.names()
@@ -24,13 +24,13 @@ test.names <- function(names, data=X){ # Function to check if name is  the List
 # function to choose "real" name of all species (with subsp. / aggr. / author and all that Jazz!)
 ## You can write 'pause' to pause the process and proceed by inserting the obtained list with 'continue.after.pause'
 
-choose.name <- function(names, data=X, write.tmp.file=T, continue.after.pause){  
+choose.name <- function(names, data=X, write.tmp.file=T, continue.after.pause){
 	X <- data
 	X$g <- sub(" .*", "", X$Latin) # get Genus of all Species
 	pause <- F
-	
+
 	if(missing(continue.after.pause)) newnames <- as.character() else newnames <- continue.after.pause
-	
+
 	for(i in names[(length(newnames)+1):length(names)]){ # loop for all species
 	  if(pause==F){
 	    proceed <- FALSE
@@ -42,24 +42,24 @@ choose.name <- function(names, data=X, write.tmp.file=T, continue.after.pause){
   		  while(length(x)==0 & !(i %in% c("pause", "NA", as.character(0:9))) ){ # keep asking Names  till there is a good suggesetion or name is "NA" or a number (0-9) or "pause"
   		    g <-  sub(" .*", "", i)
   		    if(!(g %in% X$g)) { # if genus not found compare to all species
-  		      x <- agrep(i, X$Latin, value = TRUE, ignore.case = TRUE)  
+  		      x <- agrep(i, X$Latin, value = TRUE, ignore.case = TRUE)
   		      cat(paste('Genus "', g, '"not found\n', sep = ""))
   		    } else { #if genus is found, search only within genus
-  		      x <- agrep(i, X$Latin[X$g==g], value = TRUE, ignore.case = TRUE)  
+  		      x <- agrep(i, X$Latin[X$g==g], value = TRUE, ignore.case = TRUE)
   		    }
   			  if(length(x)==0) {
   			    cat(paste('"', i, '" was not found in List.\n', sep=""))
   			    i <- readline('Change name:')# if no similar name is found, ask for a total new name
   			  }
   		  }
-  		  
+
   		  if(i %in% c("NA", as.character(0:9))) x <- i.back # when name was "NA" or number, put in original name
-  		  
-  		  
-  		 
+
+
+
   			if(length(x)>1) { # if there are option, ask for a choice
-  			  cat(paste('"',i, '" was not found in List.\n', sep = ""))
-  			  nr <- readline(prompt = paste('Choose suggestion:\n', paste(paste(1:length(x),x, sep = " - "), collapse = '\n'),'', sep=""))
+  			  cat(paste('"',i, '" was not found in List. Alternatives:\n', paste(paste(1:length(x),x, sep = " - "), collapse = '\n'), sep = ""))
+  			  nr <- readline(prompt = paste('Choose number:' ,'', sep=""))
   			  if(nr=="pause") pause <- T else  {
   			    z <- x[as.numeric(nr)]
   			    cat(paste(z, "\n\n", sep = ""))
@@ -67,17 +67,17 @@ choose.name <- function(names, data=X, write.tmp.file=T, continue.after.pause){
   			} else {
   				z <- x
   			}
-  			
+
   		  if(pause) print("Pause!") else {
   		    newnames[length(newnames)+1] <- z
   		  }
-		 
+
   		}
 	  }
 	}
-	
+
 	if(write.tmp.file) write.csv(data.frame(oldnames=names[1:length(newnames)], newnames=newnames), "tmp.csv", row.names = F)
-	
+
 	return(newnames)
 }
 
