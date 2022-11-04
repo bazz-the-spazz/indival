c61a1d2677d1515a52221b071e5bd31fb242fc0d
bazz-the-spazz <44613732+bazz-the-spazz@users.noreply.github.com>
2022-11-04 08:59:59 +0100

    aksing for confirmation when "cf" or "sp"

--- Correct_species_names.r
+++ Correct_species_names.r
@@ -5,7 +5,6 @@
 ## You can write 'pause' to pause the process and proceed by inserting the obtained list with 'continue.after.pause'
 
 choose.name <- function(names, data=X, write.tmp.file=T, continue.after.pause){
-	names <- gsub("_", " ", names) # use blanks
 	X <- data
 	X$g <- sub(" .*", "", X$Latin) # get Genus of all Species
 	pause <- F
@@ -19,47 +18,67 @@ choose.name <- function(names, data=X, write.tmp.file=T, continue.after.pause){
 				newnames[length(newnames)+1] <- i
 			} else {
 				i.back <- i # creat backup
+				i <- gsub("\\."," ", i) # get rid of points
+				i <- gsub("_"," ", i)   # get rid of underscores
+				i <- paste(i, " ", sep="") # add a blank at the end
+				i <- gsub("  "," ", i)   # get rid of double blanks
 				x <- character()  # x will be the new name
+
+				toggle <- F # a switch for when Species name is changed
+
 				while(length(x)==0 & !(i %in% c("pause", "zero", "NA", as.character(0:9))) ){ # keep asking Names  till there is a good suggesetion or name is "NA" or a number (0-9) or "pause"
+					if(toggle) i.back <- i  # change species name when switch is on
 					g <-  sub(" .*", "", i)
+
 					if(!(g %in% X$g)) { # if genus not found compare to all species
 						x <- agrep(i, X$Latin, value = TRUE, ignore.case = TRUE)
 						cat(paste('Genus "', g, '" not found\n', sep = ""))
-						if(length(x)==1) cat(paste('"', i, '" was corrected to "',x, '"\n', "\n", sep = "" ))
+						if(length(x)==1) cat(paste('"', i.back, '" was corrected to "',x, '"\n', "\n", sep = "" ))
 					} else { #if genus is found, search only within genus
 						x <- agrep(i, X$Latin[X$g==g], value = TRUE, ignore.case = TRUE)
 					}
-					if(length(x)==0) {
-						cat(paste('"', i, '" was not found in List.\n', sep=""))
-						i <- readline('Change name (zero to keep original):')# if no similar name is found, ask for a total new name
+
+					if(length(x)==0) { # when species was not found, ask for a new name
+						cat(paste('"', i.back, '" was not found in List.\n', sep=""))
+						i <- readline('Enter a new name (zero to keep original):')# if no similar name is found, ask for a total new name
+						if(!(i %in% c("zero", 0, "NA"))) toggle <- T  # toggle the switch when a new name is typed in
 					}
 				}
 
-				if(i %in% c("NA", "zero", as.character(0:9))) x <- i.back # when name was "NA" or number, put in original name
+				# when name was "NA" or number, put in original name
+				if(i %in% c("NA", "zero", as.character(0:9))) x <- i.back
 
-
+				# When only one option: check if there is "cf" or "sp" in the name before automatically correcting.
 				if(length(x)==1) {
-					if(length(grep(" cf", i, ignore.case = T))>0){
-						cat(paste('\n"cf" detected in "',i, '". What do we do?', '\n1. Change to "', x, '?\n2. Keep "', i, '"?', sep="" ))
-						nr <- readline(prompt = paste('\nChoose number:' ,'', sep=""))
-						x <- ifelse(nr==1, x, i)
+					if(length(grep(" cf ", i, ignore.case = T))>0){ # if there is a cf
+						cat(paste('\n"cf" detected in "',i.back, '". What do we do?', '\n1. Change to "', x, '?\n2. Keep "', i.back, '"?', sep="" ))
+						nr <- 0
+						while(!(nr %in% 1:2)) {
+							nr <- readline(prompt = paste('Choose number:' ,'', sep=""))
+							if(!(nr %in% 1:2))	cat("Sorry, do it again!")
+						}
+						x <- ifelse(nr==1, x, i.back)
 					}
-					if(length(grep(" sp", i, ignore.case = T))>0){
-						cat(paste('\n"sp" detected in "',i, '". What do we do?', '\n1. Change to "', x, '?\n2. Keep "', i, '"?', sep="" ))
-						nr <- readline(prompt = paste('\nChoose number:' ,'', sep=""))
-						x <- ifelse(nr==1, x, i)
+					if(length(grep(" sp ", i, ignore.case = T))>0){  # if there is a sp
+						cat(paste('\n"sp" detected in "',i, '". What do we do?', '\n1. Change to "', x, '?\n2. Keep "', i.back, '"?', sep="" ))
+						nr <- 0
+						while(!(nr %in% 1:2)) {
+							nr <- readline(prompt = paste('Choose number:' ,'', sep=""))
+							if(!(nr %in% 1:2))	cat("Sorry, do it again!")
+						}
+						x <- ifelse(nr==1, x, i.back)
 					}
-					if(x!=i)cat(paste('"', i, '" was corrected to "',x, '"\n', "\n", sep = "" ))
+					if(x!=i.back)cat(paste('"', i.back, '" was corrected to "',x, '"\n', "\n", sep = "" ))
 				}
 
 
 				if(length(x)>1) { # if there are option, ask for a choice
-					cat(paste('"',i, '" was not found in List. Alternatives:\n', paste(paste(1:length(x),x, sep = " - "), collapse = '\n'), sep = ""))
-					nr <- readline(prompt = paste('\nChoose number (zero to keep original):' ,'', sep=""))
+					cat(paste('"',i.back, '" was not found in List. Alternatives:\n', paste(paste(1:length(x),x, sep = " - "), collapse = '\n'), sep = ""))
+					nr <- readline(prompt = paste('Choose number (zero to keep original):' ,'', sep=""))
 					if(nr=="pause") pause <- T else  {
 						if(nr %in% c("zero", 0)){
 							z <- i.back
-							cat(paste('"', i, '" was kept.\n', "\n", sep = "" ))
+							cat(paste('"', i.back, '" was kept.\n', "\n", sep = "" ))
 						} else {
 							z <- x[as.numeric(nr)]
 							cat(paste('"', i, '" was corrected to "',z, '"\n', "\n", sep = "" ))
